660adffed75e9bf3fba8ec1af6034a8df92acf24
Felipe Burgos <burgosfelipeaa@gmail.com>
2017-06-16 15:59:27 -0400

    utilizacion de v-for

--- js/vue.js
+++ js/vue.js
@@ -1,1 +1,23 @@
-console.log(Vue);
+new Vue ({
+	el: '#main',
+	data: {
+		persona: {
+			nombre: 'Juan',
+			profesion: 'dev',
+			ciudad: 'Valencia'
+		},
+		
+		numeros: [
+			1,2,3,4,5
+		],
+		semana: [
+			{nombre: 'Lunes', numero: 1},
+			{nombre: 'Martes', numero: 2},
+			{nombre: 'Miércoles', numero: 3},
+			{nombre: 'Jueves', numero: 4},
+			{nombre: 'Viernes', numero: 5},
+			{nombre: 'Sábado', numero: 6},
+			{nombre: 'Domingo', numero: 7},
+		]
+	}
+});
