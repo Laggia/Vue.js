a4f520d3b6b0d1bca2d7c4a63dea5324aec03a71
Felipe Burgos <burgosfelipeaa@gmail.com>
2017-06-18 20:05:14 -0400

    Utilizacion de AJAX con Vue resource y peticones a randomuser.me/api

--- js/vue.js
+++ js/vue.js
@@ -1,58 +1,26 @@
-Vue.component('cita', {
-	template: '\
-		<div class="panel panel-primary">\
-			<div class="panel-body">\
-				<h1 v-text="citaElegida"></h1>\
-			</div>\
-			<div class="panel-footer">\
-				<button type="button" @click="sumarVoto(true)" class="btn btn-success">Me Gusta | {{ votosSi }}</button>\
-				<button type="button" @click="sumarVoto(false)" class="btn btn-danger">No me Gusta | {{ votosNo }}</button>\
-			</div>\
-		</div>\
-	',
-	mounted: function(){
-		this.elegirCita();
-	},
-
-	data: function(){
-		return {
-			votosSi: 0,
-			votosNo: 0,
-
-			citaElegida: '',
-		
-			citas: [
-				'"It might help if we ran the MBAs out of Washington." -- Admiral Grace',
-				'"A mind is a terrible thing to have leaking out yours ears." -- The League',
-				'"Only the hypocrite is really rotten to the core" -- Hannah Arendt.',
-				'"Yo baby yo baby yo." -- Eddie Murphy',
-				'A morsel of genuine history is a thing so rare as to be always valuable. -- Thomas Jefferson'
-			],
-
-		};
-	},
-
-	methods: {
-		elegirCita: function(){
-			var cita = this.citas[Math.floor(Math.random() * this.citas.length)];
-
-			this.citaElegida = cita;
-		},
-
-		sumarVoto: function(voto){
-			if(voto)
-				this.votosSi++;
-			else
-				this.votosNo++;
-		},
-		restarVoto: function(){
-			if (this.votos > 0)
-			this.votos--;
-		}
-	}
-});
-
+var urlAPI = 'https://randomuser.me/api/?results=50';
 new Vue ({
-	el: '#main',	
-
+	el: '#main',
+	created: function(){
+		this.consultarAPI();
+	},
+	data: {
+		personas: [],
+	},
+	methods: {
+		consultarAPI: function(){
+			this.$http.get(urlAPI)
+				.then(function(respuesta){
+					this.personas =respuesta.data.results;
+				});
+		}
+	},
+	components: {
+		'personas': {
+			template: '<div><ul v-for="persona in lista">\
+					<li>{{ persona.name.first }}</li>\
+				</ul></div>',
+			props: ['lista']		
+		},
+	},
 });
