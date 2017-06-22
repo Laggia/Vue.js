40e641e97072906e498220b815e229c5098b7f1e
Felipe Burgos <burgosfelipeaa@gmail.com>
2017-06-22 03:21:42 -0400

    Separacion a archivo js

--- js/vue.js
+++ js/vue.js
@@ -1,1 +1,38 @@
-console.log(Vue);
+new Vue ({
+			el: '#app',
+			data:{
+				newTask: '',
+				tasks:[
+					{
+						task: 'Lo mismo primero',
+						pending: true
+					},
+					{
+						task: 'Lo mismo primero',
+						pending: false
+					},
+					{
+						task: 'Lo mismo primero',
+						pending: true
+					},
+				]
+			},
+			computed: {
+				tareasPendientes: function(){
+					return this.tasks.filter(function(item){
+						return item.pending;
+					});
+				}				
+			},
+			methods:{
+				createTask: function(event){
+					event.preventDefault();
+					this.tasks.push({
+						task: this.newTask,
+						pending: false,
+					});
+					this.newTask ='';
+				}
+			},
+
+		});
